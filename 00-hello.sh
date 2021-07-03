91abd13e88794cc1c7aa54f1dfbf8fca4a20daac
Monmoy042 <monmoykms.16@gmail.com>
2021-07-03 12:45:00 +0600

    even odd problem added

--- 00-hello.sh
+++ 00-hello.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 echo "Hello World!"
 
+# system variable
+echo $SHELL
+echo $BASH 
+echo $PWD
+
+# User defined or custom variable
+name="Monmoy"
+echo "My name is $name" 
+
+# User input
 echo "Enter a number"
 read num
 echo "The entered number is: $num"
---
+++ 01-problem1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -e "Enter a number: \c"
+read n
+evenOdd=$(( n%2 ))
+if [ $evenOdd -eq 0 ]
+then
+    echo "You entered an even number."
+else
+    echo "You entered an odd number."
+fi
