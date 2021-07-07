888df64c39e5969f981de060755de1248fec9038
Monmoy042 <monmoykms.16@gmail.com>
2021-07-07 12:00:05 +0600

    Rename multiple files

---
+++ fileOperation/multipleFiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo -e "Enter the name of the files: \c"
+read fileName
+echo -e "How many file you want to create \c"
+read fileNumber
+
+i=0
+for (( i=1; i<=$fileNumber ; i++ ))
+do
+    touch $fileName-$i.txt
+done
+echo "Files has been created successfully!!"
