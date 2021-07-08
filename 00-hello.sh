e7915a0eca570a5973bac93294cb9b0104ab2ef7
Monmoy042 <monmoykms.16@gmail.com>
2021-07-08 12:34:25 +0600

    hello.sh file modified

--- 00-hello.sh
+++ 00-hello.sh
@@ -3,14 +3,34 @@ echo "Hello World!"
 
 # system variable
 echo $SHELL
-echo $BASH 
+echo $BASH
 echo $PWD
 
 # User defined or custom variable
 name="Monmoy"
-echo "My name is $name" 
+echo "My name is $name"
 
 # User input
 echo "Enter a number"
-read num
+# read num
 echo "The entered number is: $num"
+
+# Function
+function Hello(){
+    echo "Hello World!"
+}
+
+Quit () {
+    exit
+}
+
+# Calling the Hello and Quit functions
+Hello
+echo "Hello there!"
+# Quit
+
+# function with argument
+function print() {
+    echo $1 $2 $3 $4
+}
+print Welcome to linux world
