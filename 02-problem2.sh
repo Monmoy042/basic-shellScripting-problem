e3c2349a2b6262607a6f81d7f19e82f46edfe1fe
Monmoy042 <monmoykms.16@gmail.com>
2021-07-10 11:54:44 +0600

    Arithmatic operator added

--- 02-problem2.sh
+++ 02-problem2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 : '
 echo -e "Enter an integer: \c"
-read num1 
+read num1
 echo -e "Enter an integer: \c"
 read num2
 
@@ -21,16 +21,72 @@ echo "The division of $num1 and $num2 is:$div"
 mod=$(( $num1 % $num2 ))
 echo "The modulus of $num1 and $num2 is:$mod"
 '
+# num1=20
+# num2=10
+# sum=$( expr $num1 + $num2 )
+# echo $sum
+# sub=$( expr $num1 - $num2 )
+# echo $sub
+# pro=$( expr $num1 \* $num2 )
+# echo $pro
+# div=$( expr $num1 / $num2 )
+# echo $div
+
+# conert hexadecimal to decimal
+# read hex
+# echo -n "Hexadecimal number for $hex is: "
+# echo "obase=10;ibase=16;$hex" | bc
+
 
 # In this method we cannot do any arithmetic operations using floating number
 
 # Arithmetic operator
 a=10
 b=20
-if [ $a == $b ]
+# Equal
+if (( $a == $b ))
 then
     echo "a is equal to b"
 else
     echo "a is not equal to b"
 fi
-echo 
+
+# Not equal
+if (( $a != $b ))
+then
+    echo "a is not equal to b"
+else
+    echo "a is equal to b"
+fi
+
+# Greater than
+if (( $a > $b ))
+then
+    echo "a is greater than b"
+else
+    echo "a is not greater than b"
+fi
+
+# Less than
+if (( $a < $b ))
+then
+    echo "a is less than b"
+else
+    echo "a is not less than b"
+fi
+
+# Greater than or equal
+if (( $a >= $b ))
+then
+    echo "a is greater than or equal than b"
+else
+    echo "a is not greater than or equal than b"
+fi
+
+# Less than or equal
+if (( $a <= $b ))
+then
+    echo "a is less than or equal than b"
+else
+    echo "a is not less than or equal than b"
+fi
