aa606e9dafc49cf1ae5cb4fcc9bb814fc4c8fe8b
Monmoy042 <monmoykms.16@gmail.com>
2021-07-22 20:40:32 +0600

    Simple Calculator Version 1.0.2 updated

--- 04-problem.sh
+++ 04-problem.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 # Simple Calculator Script
-printf "Which operation you want to do? \n 1)Addition \n 2)Substraction \n 3)Multiplication \n 4)Division \n 5)Reaminder \n 6)Square Root \n"
+printf "Which operation you want to do? \n 1)Addition \n 2)Substraction \n 3)Multiplication \n 4)Division \n 5)Reaminder \n 6)Square Root \n 7)Square \n 8)Cube \n"
 read option
 res=0
 
 # Input two numbers using function
 input() {
-    echo -e "Enter a number: \c"
+    echo -e "Enter 1st number: \c"
     read num1 
-    echo -e "Enter another number: \c"
+    echo -e "Enter 2nd number: \c"
     read num2
+}
+# Input 1 number using function
+input2() {
+    echo -e "Enter 1st number: \c"
+    read num1
 }
 
 if [ $option -eq 1 ]
@@ -44,11 +49,22 @@ then
 
 elif [ $option -eq 6 ]
 then
-    echo -e "Enter a number: \c"
-    read num1 
+    input2
     res=` echo " scale=2;sqrt($num1) " |bc -l`
     echo "Square root of $num1 is: $res"
 
+elif [ $option -eq 7 ]
+then
+    input2
+    res=` echo " scale=2;$num1^2 " |bc -l`
+    echo "Square of $num1 is: $res"
+
+elif [ $option -eq 8 ]
+then
+    input2
+    res=` echo " scale=2;$num1^3 " |bc -l`
+    echo "Square of $num1 is: $res"
+
 else 
-    echo "You entered wring input!!!"
+    echo "You entered wrong input!!!"
 fi
